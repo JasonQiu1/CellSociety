3884ca59343b7bafccf681340e2cd0d5b2434e1c
Jason Qiu <jq48@duke.edu>
2024-01-29 14:17:23 -0500

    design [DESIGN-09_CS-10]: set up controller boilerplate in its own package

---
+++ src/main/java/cellsociety/controller/Controller.java
@@ -0,0 +1,26 @@
+package cellsociety.controller;
+
+import cellsociety.model.Simulation;
+import java.awt.event.KeyEvent;
+
+/**
+ * Responsible for telling Game or Simulation what to do given user input. Doesn't need to be
+ * instantiated.
+ *
+ * @author Jason Qiu (jq48)
+ */
+public class Controller {
+  /**
+   * Handles the given key event.
+   * @param keyEvent the key event.
+   */
+  public static void handleKeyEvents(KeyEvent keyEvent) {
+
+  }
+
+  /**
+   * No initialization required, doesn't hold state.
+   */
+  private Controller() {
+  }
+}
