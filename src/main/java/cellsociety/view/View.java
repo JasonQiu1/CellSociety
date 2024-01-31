f7c865344df44175ecf971c34bb17084a9717e6a
Jason Qiu <jq48@duke.edu>
2024-01-31 14:23:14 -0500

    feat [DESIGN-08_CS-6]: view initialization - split stage for uidrawer and griddrawer

--- src/main/java/cellsociety/view/View.java
+++ src/main/java/cellsociety/view/View.java
@@ -2,6 +2,11 @@ package cellsociety.view;
 
 import cellsociety.model.Grid;
 import cellsociety.model.Simulation;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
 
 /**
@@ -12,6 +17,14 @@ import javafx.stage.Stage;
  */
 public class View {
 
+  private static int STAGE_WIDTH = 750;
+  private static int STAGE_HEIGHT = 500;
+  private static Paint STAGE_COLOR = Color.WHITE;
+  private static double GRID_TO_UI_RATIO = 0.8;
+  private Stage mainScene;
+  private UserInterfaceDrawer uiDrawer;
+  private GridDrawer gridDrawer;
+
   /**
    * Splits the given Stage into two portions for the cell grid and the user interface. Also hooks
    * user key events and user interface buttons and text fields to their handlers in Controller.
@@ -19,6 +32,17 @@ public class View {
    * @param stage the stage to draw everything on.
    */
   public View(Stage stage) {
+    Pane gridPane = new Pane();
+    gridPane.setMaxSize(STAGE_WIDTH, STAGE_HEIGHT * GRID_TO_UI_RATIO);
+    gridDrawer = new GridDrawer(gridPane);
+
+    Pane uiPane = new Pane();
+    uiPane.setMaxSize(STAGE_WIDTH, STAGE_HEIGHT * (1 - GRID_TO_UI_RATIO));
+    uiDrawer = new UserInterfaceDrawer(uiPane);
+
+    Group sceneRoot = new Group(uiPane, gridPane);
+    Scene scene = new Scene(sceneRoot, STAGE_WIDTH, STAGE_HEIGHT, STAGE_COLOR);
+    stage.setScene(scene);
   }
 
   /**
