c730ec4c1d137c52f01fa05c55ad67118474653e
Jason Qiu <jq48@duke.edu>
2024-02-01 10:48:46 -0500

    feat [DESIGN-08]: implemented GridDrawer, which holds on to a GridView class that represents the view of the model's Grid

---
+++ src/main/java/cellsociety/view/GridView.java
@@ -0,0 +1,73 @@
+package cellsociety.view;
+
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Paint;
+
+/**
+ * Responsible for representing the view of a grid.
+ *
+ * @author Jason Qiu
+ */
+public class GridView {
+
+  GridPane gridPane;
+  CellView[][] cellViewGrid;
+  int numRows;
+  int numColumns;
+
+  /**
+   * Creates a new fixed array of CellView instances parented to a GridPane instance.
+   *
+   * @param numRows    the number of rows of the grid view.
+   * @param numColumns the number of columns of the grid view.
+   */
+  public GridView(int numRows, int numColumns) {
+    this.numRows = numRows;
+    this.numColumns = numColumns;
+
+    initializeGrid(numRows, numColumns);
+  }
+
+  /**
+   * Initializes the gridPane and cellViewGrid at the same time.
+   *
+   * @param numRows    the number of rows.
+   * @param numColumns the number of columns.
+   */
+  private void initializeGrid(int numRows, int numColumns) {
+    gridPane = new GridPane();
+    cellViewGrid = new CellView[numRows][numColumns];
+    for (int row = 0; row < numRows; row++) {
+      for (int col = 0; col < numRows; col++) {
+        cellViewGrid[row][col] = new CellView();
+        gridPane.getChildren().add(cellViewGrid[row][col]);
+        GridPane.setRowIndex(cellViewGrid[row][col], row);
+        GridPane.setColumnIndex(cellViewGrid[row][col], col);
+      }
+    }
+  }
+
+  public GridPane getGridPane() {
+    return gridPane;
+  }
+
+  public int getNumRows() {
+    return numRows;
+  }
+
+  public int getNumColumns() {
+    return numColumns;
+  }
+
+  /**
+   * Set the color of a cell view.
+   *
+   * @param row   the row of the cell view.
+   * @param col   the column of the cell view.
+   * @param color the new color to set the cell view to.
+   */
+  public void setCellViewColor(int row, int col, Paint color) {
+    cellViewGrid[row][col].setColor(color);
+  }
+}
+
