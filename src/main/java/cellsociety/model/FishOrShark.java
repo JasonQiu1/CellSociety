83841ca06b76318a12242a0786fb9e1acce78a44
Spencer Katz <sek49@duke.edu>
2024-02-05 17:43:32 -0500

    Small Change for FishOrShark

--- src/main/java/cellsociety/model/FishOrShark.java
+++ src/main/java/cellsociety/model/FishOrShark.java
@@ -2,14 +2,11 @@ package cellsociety.model;
 
 public class FishOrShark extends Cell {
   private int chrononsSurvived;
-
   private int energy;
 
-  private int START_ENERGY = 10;
   public FishOrShark(int currentState, int START_ENERGY) {
     super(currentState);
     chrononsSurvived = 0;
-    this.START_ENERGY = START_ENERGY;
     energy = START_ENERGY;
   }
   public int getChrononsSurvived() {
